b9a15217ec505f94aa7c687395bf65205651444d
Marek Borovsky <borovsky@oksystem.cz>
2019-10-16 11:50:04 +0200

    Frontend - Footer v1

--- frontend/src/layout/Footer.jsx
+++ frontend/src/layout/Footer.jsx
@@ -1,23 +1,63 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core';
-import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 const styles = theme => ({
     footer : {
-        padding: '3rem 1rem',
+        padding: '3rem 10vw',
         marginTop: 'auto',
         backgroundColor : '#242424',
         color : '#fff',
         textAlign : 'center'
+    },
+    left : {
+        textAlign : 'left'
+    },
+    link : {
+        textDecoration : 'none'
+    },
+    nativeLink : {
+        textDecoration : 'none',
+        color : '#fff',
+        '&:active' : {
+            textDecoration : 'none',
+            color : '#fff'
+        },
+        '&:visited' : {
+            textDecoration : 'none',
+            color : '#fff'
+        }
     }
 });
 
 const Footer = ({ classes }) => (
     <footer className={classes.footer}>
-        <Container maxWidth="sm">
-            <Typography variant="body2" color="secondary">TODO - footer</Typography>
-        </Container>
+        <Grid container>
+            <Grid item xs={12} sm={6} className={classes.left}>
+                <Link to="/terms" className={classes.link}>
+                    <Typography variant="body2" color="secondary">Obchodní podmínky</Typography>
+                </Link>
+                <Link to="/faq" className={classes.link}>
+                    <Typography variant="body2" color="secondary">FAQ</Typography>
+                </Link>
+                <Link to="/contact" className={classes.link}>
+                    <Typography variant="body2" color="secondary">Kontakt</Typography>
+                </Link>
+                <a href="todo-facebook" className={classes.nativeLink}>
+                    <FacebookIcon />
+                </a>
+                <a href="todo-instagram" className={classes.nativeLink}>
+                    <InstagramIcon />
+                </a>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                &copy; 2019
+            </Grid>
+        </Grid>
     </footer>
 );
 
