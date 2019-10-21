0ce2dc43bdbd86ced561227a4f8d4f9a867e77df
Marek Borovsky <borovsky@oksystem.cz>
2019-10-21 09:20:48 +0200

    Frontend - Footer year typography

--- frontend/src/layout/Footer.jsx
+++ frontend/src/layout/Footer.jsx
@@ -69,7 +69,7 @@ const Footer = ({ classes }) => (
                 </Typography>
             </Grid>
             <Grid item xs={12} sm={6}>
-                2019
+                <Typography variant="body2">2019</Typography>
             </Grid>
         </Grid>
     </footer>
