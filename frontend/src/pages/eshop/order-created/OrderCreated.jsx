4e0b906b93c62d30a55e637597fcab19e61047f9
Marek Borovsky <borovsky@oksystem.cz>
2019-10-24 13:10:32 +0200

    Frontend - Order created page design

--- frontend/src/pages/eshop/order-created/OrderCreated.jsx
+++ frontend/src/pages/eshop/order-created/OrderCreated.jsx
@@ -1,44 +1,65 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
 import PaymentQR from './components/PaymentQR';
 import PaymentInfo from './components/PaymentInfo';
+import { withStyles } from '@material-ui/styles';
+import Alert from 'components/Alert';
 
-const OrderCreated = () => (
+const styles = theme => ({
+    block : {
+        marginBottom : '2rem'
+    },
+    qrCodeWrapper : {
+        textAlign : 'center'
+    }
+});
+
+const OrderCreated = ({ classes }) => (
     <>
-        <Typography variant="body1">
-            Děkuji,<br />
+        <Alert type="success">Objednávka byla úspěšně vytvořena</Alert>
+        <Typography variant="body1" className={classes.block}>
+            Děkuji,<br /><br />
             Vaše objednávka byla úspěšně potvrzena. Zboží expedujeme po připsání platby
             na účet. Pokud platba nebude připsána do 5 pracovních dnů, objednávka bude
-            automaticky zrušena.<br />
-            Údaje pro platbu:
-            <PaymentInfo paymentData={{
-                accountNumber : '123456789',
-                bankCode : '0000',
-                variableSymbol : '999999999',
-                amount : 1324,
-                currency : 'CZK',
-                message : 'Toto je test...'
-            }} />
-            <PaymentQR paymentData={{
-                accountNumber : '123456789',
-                bankCode : '0000',
-                variableSymbol : '999999999',
-                amount : 1324,
-                currency : 'CZK',
-                message : 'Toto je test...'
-            }} />
+            automaticky zrušena.
         </Typography>
         <Typography variant="body1">
+            Údaje pro platbu:
+        </Typography>
+        <Grid container className={classes.block} alignItems="center">
+            <Grid item xs={12} md={6}>
+                <PaymentInfo paymentData={{
+                    accountNumber : '222885',
+                    bankCode : '1234',
+                    variableSymbol : '999999999',
+                    amount : 1324,
+                    currency : 'CZK',
+                    message : 'Toto je test...'
+                }} />
+            </Grid>
+            <Grid item xs={12} md={6} className={classes.qrCodeWrapper}>
+                <PaymentQR paymentData={{
+                    accountNumber : '222885',
+                    bankCode : '1234',
+                    variableSymbol : '999999999',
+                    amount : 1324,
+                    currency : 'CZK',
+                    message : 'Toto je test...'
+                }} />
+            </Grid>
+        </Grid>
+
+        <Typography variant="body1" className={classes.block}>
             Informace o potvrzení objednávky byla automaticky zaslána na Vámi uvedenou
-            emailovou adresu spolu s platebními údaji. Po dokončení objednácky a odeslání
+            emailovou adresu spolu s platebními údaji. Po dokončení objednávky a odeslání
             zboží přepravci, kterého jste zvolili, Vám v rámci ochrany životního prostředí
             zašleme fakturu pouze v elektronické podobě.
         </Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" className={classes.block}>
             Děkuji za Vaši objedávku<br />
             a přeji Vám, aby Vám náš produkt pomohl v organizaci Vaší domácnosti.
         </Typography>
     </>
 );
 
-export default OrderCreated;
+export default withStyles(styles)(OrderCreated);
--- frontend/src/pages/eshop/order-created/components/PaymentQR.jsx
+++ frontend/src/pages/eshop/order-created/components/PaymentQR.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { paymentPropTypes } from './payment.prop-types';
 
 const PaymentQR = ({ paymentData }) => (
-    <img src={`https://api.paylibo.com/paylibo/generator/czech/image?accountNumber=${paymentData.accountNumber}&bankCode=${paymentData.bankCode}&vs=${paymentData.variableSymbol}&amount=${paymentData.amount}&currency=${paymentData.currency}&message=${paymentData.message}`}
+    <img src={`https://api.paylibo.com/paylibo/generator/czech/image?accountNumber=${paymentData.accountNumber}&bankCode=${paymentData.bankCode}&vs=${paymentData.variableSymbol}&amount=${paymentData.amount}&currency=${paymentData.currency}&message=${paymentData.message}&size=200`}
          alt="QR kód"
          title="QR kód pro platbu"
     />
