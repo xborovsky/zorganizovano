25b1a26e5b290113c93f45f519da41102b0123c7
xborovsky <xborovsky@gmail.com>
2019-10-29 19:59:22 +0100

    Backend - HTML email

--- backend/src/main/java/cz/zorganizovano/backend/email/EmailServiceImpl.java
+++ backend/src/main/java/cz/zorganizovano/backend/email/EmailServiceImpl.java
@@ -1,9 +1,11 @@
 package cz.zorganizovano.backend.email;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,11 +16,17 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void send(String to, String subject, String text) throws MailException {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        emailSender.send(message);
+        try {
+            MimeMessage mimeMessage = emailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(to);
+            messageHelper.setSubject(subject);
+            messageHelper.setText(text, true);
+            
+            emailSender.send(mimeMessage);
+        } catch (MessagingException ex) {
+            //throw new MailException(ex); TODO
+        }
     }
 
 }
