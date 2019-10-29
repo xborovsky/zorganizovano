57ecca5dc0b3b0d957fc39ef3772019a5a070bc4
Marek Borovsky <borovsky@oksystem.cz>
2019-10-29 13:33:12 +0100

    Frontend - breadcrumbs -> tip detail

--- frontend/src/pages/tips/tip-detail/TipDetail.jsx
+++ frontend/src/pages/tips/tip-detail/TipDetail.jsx
@@ -3,23 +3,40 @@ import PropTypes from 'prop-types';
 import { Paper, Typography } from '@material-ui/core';
 import ReactHtmlParser from 'react-html-parser';
 import { withStyles } from '@material-ui/styles';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import { Link } from 'react-router-dom';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 const styles = theme => ({
     root : {
         padding : '1rem 2rem'
+    },
+    breadcrumbsWrapper : {
+        justifyContent: 'center',
+        flexWrap: 'wrap'
     }
 });
 
 const TipDetail = ({ tip, classes }) => {
 
     return (
-        <Paper className={classes.root}>
-            <Typography variant="h1">{ tip.title }</Typography>
-            <span>{ tip.publishedFormatted }</span>
-            <Typography variant="body1" component="div">
-                { ReactHtmlParser(tip.content) }
-            </Typography>
-        </Paper>
+        <>
+            <div className={classes.breadcrumbsWrapper}>
+                <Paper elevation={0}>
+                    <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />}>
+                        <Link to="/tips">Tipy</Link>
+                        <Typography>{ tip.title }</Typography>
+                    </Breadcrumbs>
+                </Paper>
+            </div>
+            <Paper className={classes.root}>
+                <Typography variant="h1">{ tip.title }</Typography>
+                <span>{ tip.publishedFormatted }</span>
+                <Typography variant="body1" component="div">
+                    { ReactHtmlParser(tip.content) }
+                </Typography>
+            </Paper>
+        </>
     );
 };
 
