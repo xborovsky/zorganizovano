f5a5939e1bd958d9afd9cf2bb30dd148f9929078
xborovsky <xborovsky@gmail.com>
2019-11-08 18:23:20 +0100

    Frontend - rest url based on environment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,9 +11,11 @@ var isAbsoluteURLRegex = /^(?:\w+:)\/\//;
 axios.interceptors.request.use(config => {
   // Concatenate base path if not an absolute URL
   if ( !isAbsoluteURLRegex.test(config.url)) {
+    const urlPrefix = process.env.NODE_ENV === "production" ? 
+      "https://zorganizovano.cz:" : "http://localhost:";
     const port = config.url.startsWith('/img-api') ? 8082 : 8081;
     const defaultContextPath = port === 8081 ? '/api' : '';
-    config.url = join(`http://localhost:${port}${defaultContextPath}`, config.url);
+    config.url = join(`${urlPrefix}${port}${defaultContextPath}`, config.url);
   }
 
   return config;
