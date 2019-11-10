28ec10472593b3d38ac69d7d72fa7d38e50b01f5
xborovsky <xborovsky@gmail.com>
2019-11-10 17:33:20 +0100

    Price update

--- initial_data.sql
+++ initial_data.sql
@@ -3,7 +3,7 @@ SET autocommit=OFF;
 START TRANSACTION;
 
 insert into zorganizovano.items (name, subname, description, price, discount_price)
-values ('KALENDÁŘ 2020 - LIŠČÍ ROK', 'Kalendář na lednici s magnetickou lištou', 'Mějte plány celé své rodiny vždy na očích, na nejfregventovanějším místě v domácnosti - lednici.
+values ('KALENDÁŘ 2020 - LIŠČÍ ROK', 'Kalendář na lednici s magnetickou lištou', 'Mějte plány celé své rodiny vždy na očích, na nejfrekventovanějším místě v domácnosti - lednici.
 Nezapomenete na žádné vystoupení ani zápas svých dětí, partner vás nepřekvapí návštěvou kamaráda, o které vám ale určitě říkal a váš zubař vás bude chválit za pravidelné preventivní
 prohlídky. Děti uvidí, které dny jste vytíženi a které plánujete pro rodinu, kdy se chystá výlet, dovolená a prázdniny u babičky a to v celoměsíčním přehledu - takže se vám už po 
 otočení strany na další týden nestane, že už máte být na cestě na schůzku a ne doma v pyžamu s kávou v ruce. Udělejte si z organizace času ve vaší rodině příjemnou rutinu, 
@@ -18,7 +18,7 @@ mějte to zorganizováno 🙂
 <li>Státní svátky v ČR a vybrané mezinárodní dny</li>
 <li>Pohyb Měsíce (ať víte, kdy se blíží úplněk a tedy zpravidla, alespoň u nás, dětmi probdělá noc)</li>
 <li>Magnetická lišta k umístění na lednici, magnet je i ve spodní části kalendáře, při otevření lednice tedy nikam neuletí</li>
-</ul>', 299, null);
+</ul>', 349, null);
 
 insert into zorganizovano.item_details (item_id, `key`, `value`, priority_order)
 values (1, 'Rozměry', '297 x 514 x 10 mm', 1000),
@@ -26,7 +26,7 @@ values (1, 'Rozměry', '297 x 514 x 10 mm', 1000),
 (1, 'Váha', '120g', 100);
 
 insert into zorganizovano.stock_items (item_id, quantity, display_on_eshop, thumbnail_location)
-values (1, 10, true, 'products/p_1.jpg');
+values (1, 10, true, 'https://zorganizovano.cz/img-server/img/products/p_1.jpg');
 
 insert into zorganizovano.stock_item_pictures (stock_item_id, is_main, src)
 values (1, true, 'products/p_1.jpg');
@@ -41,15 +41,15 @@ values(
  curdate(), 
 '<p>Kéžbych věděla :D Ne vážně, před narozením dětí jsem o sobě smýšlela jako o někom, kdo má vše dokonale zorganizované, dokonce jsem to na pracovních pohovorech vyzdvihovala jako mou snad největší přednost. Tak to bylo tehdy. V období "před dětmi". Potom přišlo období "po Marečkovi", no tak to byl samosebou sešup. Vůbec, ale vůbec jsem netušila co dělat, jak to dělat a proč. Názorů okolo spousta, dobrých rad ještě víc. No po čase jsem si našla systém, jak se z ničeho nic starat o někoho jiného než sebe. O někoho, kdo na mě byl absolutně závislý. Do toho jsme čerstvě postavili dům a založili zahradu, no nebylo toho z ničeho nic málo. Zaběhli jsme se, fungovalo to, no stále jsem si říkala, jak se tohle dá všechno stíhat? Znáte to, domácnost vám padá na hlavu, s tím drobkem chcete trávit nějaký čas, no zachce se vám zpět mezi dospělé, tak vezmete částečný úvazek v práci, najednou se chystají Vánoce, dítko se začne v noci více budit.. no tak co uděláte? ano, rozmyslíte se, že byste rádi druhé dítě, ať přeci k sobě mají blízko. Tak mají - rok a 10 měsíců ;) a nastane období "po Sárince" a najednou všechno šlape, i když děti v noci vstávají, i když mám více povinností okolo domácnosti, i když mají oba rozdílné nároky, i když má Mareček pět kroužků v týdnu. Jak je to možné? Popravdě - není to růžové, Mareček už je ve věku kdy nad věcmi více přemýšlí a tak kuje pikle a zkouší na mě a Máru kde co, tak ano, zatím jsme se nezbláznili, ani nezačali holdovat alkoholu. Tohle je určitě na tom všem to nejnáročnější a lhala bych, kdybych vám tady teď napsala, jak tohle krásně zvládáme a že je u nás jen slunečno. Ne to ne. Jsme normální rodina, s normálními radostmi a strastmi, ale máme systém. A to nám pomáhá. Šetřit čas, šetřit energii a šetřit duševní zdraví.</p>
 <div class="blog-post-img-block">
-<img class="blog-post-img" src="http://localhost:8082/img-api/img/blog/b_2.jpg/{screenWidth};widthPct={widthPct};dpr={dpr}" alt="Jak si zorganizovat čas? 1/3" />
-<img class="blog-post-img" src="http://localhost:8082/img-api/img/blog/b_3.jpg/{screenWidth};widthPct={widthPct};dpr={dpr}" alt="Jak si zorganizovat čas? 1/3" />
+<img class="blog-post-img" src="https://zorganizovano.cz:8082/img-api/img/blog/b_2.jpg/{screenWidth};widthPct={widthPct};dpr={dpr}" alt="Jak si zorganizovat čas? 1/3" />
+<img class="blog-post-img" src="https://zorganizovano.cz:8082/img-api/img/blog/b_3.jpg/{screenWidth};widthPct={widthPct};dpr={dpr}" alt="Jak si zorganizovat čas? 1/3" />
 </div>
 <p>Mára vstává do práce každý všední den ve čtyři ráno. Ne, nepřehlédli jste se, ve 4:00 mu zvoní budík. Snídá, připravuje se, jde vyvenčit Hulka a vyráží autobusem vstříct pracovním povinnostem. Většinou se okolo tohoto času budí i Sári na ještě stále noční mléko a přebalení - pokud je to po páté hodině, už spát nejdu. Pokud dříve, mám budík denně na 6:30. Jsem typ ranní ptáče, v těchto hodinách mám nejvíce energie a tak v krátkém čase zvládnu spoustu věcí. Vysterilizuji lahvičky, dám vařit Sári novou vodu do termosky a jdu vyložit pračku, která se zapla okolo třetí ráno. Prádlo pověsím, případně poskládám co ještě schlo, chystám věci k přípravě snídaně pro mě a Marečka, když by se vzbudil dříve - bývá velmi hladový jen co vstane. Potom zvládnu svou ranní hygienu, převléknu se, ustelu a uklidím ložnici. Většinou mám čas a stíhám něco málo vyžehlit, nebo uklidit část domu, která to právě potřebuje. Nedělám téměř vůbec úklid o víkendu, snažím se o domácnost pečovat v průběhu týdne, abychom o víkendu měli čas jen sami na sebe.</p>
 <p>Mareček se budí mezi sedmou a osmou ráno, nasnídá se, podívá se na pohádku z angličtiny a pak zápasíme s oblékáním.. mezitím větrám dům a vykládám myčku (sousedí s jeho pokojem, tak mohu až když se vzbudí, spí jako zajíc). Do toho se většinou budí i Sári, kterou převléknu, obstarám a nakrmím připravenou snídaní. Chvíli zůstává v lehátku, aby se na zemi hned nepoblinkala a my si jdeme s Marečkem čistit zuby. Ráno mají děti pro sebe a vymýšlí si vlastní zábavu - Mareček se tak (snad) učí samostatnosti ve vytváření názoru a Sárinka nezávislosti. Většinou něco dělají společně a já poklízím co potřebuji a chystám oběd. Okolo desáté hodiny mám většinou vše na celý den hotovo a Sárinka jde spát a začíná nám čas jen s Marečkem. Většinou si Mareček donese nějakou oblíbenou hru sám, nebo vyberu něco, co jsme dlouho nedělali. Před obědem si ještě vezmeme dětský časopis a uděláme z něj jednu dvojstranu. Naobědváme se a Mareček jde spát. V tu chvíli mám buď čas pro sebe, nebo se už budí Sárinka a věnuji se jí. Mareček se vzbudí okolo třetí hodiny a v 15:45 je už doma Mára. Pozdraví nás a jde venčit Hulka. Buď jdeme všichni společně na procházku, nebo jdou jen kluci, nebo jedeme na kroužek. Mára si pak jde zacvičit buď do zahrady a když je pěkně, Mareček už chodí cvičit s ním, nebo si udělal malé fitko v garáži, tam jsou ale těžké činky a tak chodí raději sám.  Večeře je už buď hotová z rána nebo vaří Mára. Po večeři ukládáme Sárinku i Marečka. Sári buď usne v osm, nebo chytne druhý dech a je neuspatelná do desíti. Mareček vymýšlí, že potřebuje to či ono (však to znáte) a když není až tak unavený, podaří se mu usnout někdy po deváté - a to se snažíme, aby v půl osmé už ležel v posteli.. No a když to večer klapne, v devět obě děti spí a my se chystáme taky do postele, nebo jde alespoň Mára, protože druhý den vstává zase v ty čtyři a já se přidám, až děti usnou. Oba se párkrát v noci probudí, ale většinou je to rychlovka - Sári dám lahvičku s mlíčkem, když pije tak ji přebaluji a během dvou minut spí - nemluvím na ni, ani ji nevytahuji z postýlky. Mareček se chce většinou napít, nebo se bojí vlka (ach ty pohádky..) a tak někdy skončí u nás v posteli. No říkáme si, že takhle malej už nikdy nebude, tak ať.</p>
 <div class="blog-post-img-block">
-<img class="blog-post-img" src="http://localhost:8082/img-api/img/blog/b_4.jpg/{screenWidth};widthPct={widthPct};dpr={dpr}" alt="Jak si zorganizovat čas? 1/3" />
-<img class="blog-post-img" src="http://localhost:8082/img-api/img/blog/b_5.jpg/{screenWidth};widthPct={widthPct};dpr={dpr}" alt="Jak si zorganizovat čas? 1/3" />
-<img class="blog-post-img" src="http://localhost:8082/img-api/img/blog/b_1.jpg/{screenWidth};widthPct={widthPct};dpr={dpr}" alt="Jak si zorganizovat čas? 1/3" />
+<img class="blog-post-img" src="https://zorganizovano.cz:8082/img-api/img/blog/b_4.jpg/{screenWidth};widthPct={widthPct};dpr={dpr}" alt="Jak si zorganizovat čas? 1/3" />
+<img class="blog-post-img" src="https://zorganizovano.cz:8082/img-api/img/blog/b_5.jpg/{screenWidth};widthPct={widthPct};dpr={dpr}" alt="Jak si zorganizovat čas? 1/3" />
+<img class="blog-post-img" src="https://zorganizovano.cz:8082/img-api/img/blog/b_1.jpg/{screenWidth};widthPct={widthPct};dpr={dpr}" alt="Jak si zorganizovat čas? 1/3" />
 </div>
 <p>Naše děti jsou u nás na prvním místě a tak dbáme na to, že když jsme s nimi, jsme opravdu jen s nimi. Já v tu chvíli odkládám telefon, nedělám nic okolo domu a jsem jen s nimi. Vstávat dřív nevnímám jako nějakou oběť, chodíme spát brzy, takže nějakým spánkovým deficitem netrpíme (alespoň ne ve dnech, kdy pro vás nepřipravujeme tyto stránky ;)). Věřím tomu, že mít systém a být v plánování zorganizovaní, nám přináší spoustu času jen pro naši rodinu. Takže jestli to vnímáte stejně jako my, ráda vám sem budu psát tipy zaměřené na konkrétní témata - vaření, uklízení, péče o děti, nákupy, organizování domácnosti, plánování, organizace času, Vánoc, dovolené, ... co mě zrovna napadne nebo bude aktuální. Už teď pro vás připravuji nějaké zlepšováky a pomocníky, které my doma používáme na denní bázi, proto věřím, že by vám mohli také dobře posloužit.</p>
 <p>Dnes jsem vám chtěla přiblížit, jak fungujeme jako rodina ve všední dny a jak to tedy máme zorganizováno mezi sebou. V pokračování článku vám ukáži, jak přesně plánujeme čas.</p>
@@ -59,7 +59,7 @@ values(
 'Kéžbych věděla :D Ne vážně, před narozením dětí jsem o sobě smýšlela jako o někom, kdo má vše dokonale zorganizované, dokonce jsem to na pracovních pohovorech vyzdvihovala jako mou snad největší přednost. Tak to bylo tehdy. V období "před dětmi". Potom přišlo období "po Marečkovi", no tak to byl samosebou sešup. Vůbec, ale vůbec jsem netušila co dělat, jak to dělat a proč. Názorů okolo spousta, dobrých rad ještě víc. No po čase jsem si našla systém, jak se z ničeho nic starat o někoho jiného než sebe. O někoho, kdo na mě byl absolutně závislý. Do toho jsme čerstvě postavili dům a založili zahradu, no nebylo toho z ničeho nic málo. Zaběhli jsme se, fungovalo to, no stále jsem si říkala...',
 'blog/b_1.jpg',
 '/eshop/products/1',
-'<img src="http://localhost:8082/img-api/img/blog/b_6.jpg/{screenWidth};widthPct={widthPct};dpr={dpr}"  alt="U nás v eshopu" />');
+'<img src="https://zorganizovano.cz:8082/img-api/img/blog/b_6.jpg/{screenWidth};widthPct={widthPct};dpr={dpr}"  alt="U nás v eshopu" />');
 
 insert into zorganizovano.contact_query_types (type, sort_key) values 
 ('Potřebuji se zeptat na něco k produktu', 100),
