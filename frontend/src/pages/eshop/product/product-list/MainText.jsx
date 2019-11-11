64ebbea69ad7ddc74b06984f7567cf3015b82c99
xborovsky <xborovsky@gmail.com>
2019-11-11 20:30:46 +0100

    Remove text from eshop page

--- frontend/src/pages/eshop/product/product-list/MainText.jsx
+++ frontend/src/pages/eshop/product/product-list/MainText.jsx
@@ -13,13 +13,6 @@ const MainText = () => (
             musela dopsat všechny jmeniny i státní svátky. Letos jsem se snažila najít nějaký takový český, ale bez úspěchu. Tak jsem si ho vytvořila.
         </Typography>
         <Typography variant="body1">
-            Dvakrát do týdne u nás doma plánujeme co budeme jíst a právě tolikrát jedeme nakoupit - šetříme tak čas, životní prostředí a co si budeme povídat,
-            hlavně peníze. Vše si píšeme na papírky a ty pak lepíme na lednici, podle nich i vaříme. Původně jsem si chtěla pořídit mazací tabulku, ale to by
-            mně v našem systému nevyhovovalo. Plánujeme buď během snídaně u stolu nebo třeba na sedačce - proto pro mne byla podstatná pevná záda plánovače,
-            možnost si nákupní seznam odtrhnout a jet. Zároveň ale mít seznam kdykoliv po ruce - doteď se nám válel jeden papírek někde na kuchyňském ostrůvku.
-            Trhací lišta s magnetem to vyřešila raz dva.
-        </Typography>
-        <Typography variant="body1">
             A tak to pokračuje dál a dál :)
         </Typography>
     </MainPageText>
