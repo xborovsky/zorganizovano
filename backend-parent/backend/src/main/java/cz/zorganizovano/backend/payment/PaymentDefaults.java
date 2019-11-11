fc4794fedb8ce4110bfa3bde1d4515b873f802a5
xborovsky <xborovsky@gmail.com>
2019-11-11 20:55:28 +0100

    account number

--- backend-parent/backend/src/main/java/cz/zorganizovano/backend/payment/PaymentDefaults.java
+++ backend-parent/backend/src/main/java/cz/zorganizovano/backend/payment/PaymentDefaults.java
@@ -2,8 +2,8 @@ package cz.zorganizovano.backend.payment;
 
 public interface PaymentDefaults {
 
-    String ACCOUNT_NUMBER = "222885";
-    String BANK_CODE = "1234";
+    String ACCOUNT_NUMBER = "1957478013";
+    String BANK_CODE = "3030";
     String CURRENCY = "CZK";
 
 }
