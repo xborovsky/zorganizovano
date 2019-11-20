fe39544a358572eaedd0f5fd419d31076a57e6be
Marek Borovsky <borovsky@oksystem.cz>
2019-11-20 11:44:46 +0100

    Style order items in admin orders view

--- frontend/src/pages/admin/order-detail/components/OrderDetailItems.jsx
+++ frontend/src/pages/admin/order-detail/components/OrderDetailItems.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -8,27 +9,47 @@ import TableRow from '@material-ui/core/TableRow';
 
 import { OrderItemPropType } from './OrderItemPropType';
 import OrderDetailItemRow from './OrderDetailItemRow';
+import { makeStyles, withStyles } from '@material-ui/styles';
+
+const useStyle = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        overflowX: 'auto',
+    }
+}));
+
+const StyledTableCell = withStyles(theme => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white
+    }
+}))(TableCell);
 
 const OrderDetailItems = ({
     items
-}) => (
-    <Table>
-        <TableHead>
-            <TableRow>
-                <TableCell>Název</TableCell>
-                <TableCell>Množství</TableCell>
-                <TableCell>Cena</TableCell>
-            </TableRow>
-        </TableHead>
-        <TableBody>
-            {
-                items.map(item => (
-                    <OrderDetailItemRow item={item} key={item.id} />
-                ))
-            }
-        </TableBody>
-    </Table>
-);
+}) => {
+    const classes = useStyle();
+    return (
+        <Paper className={classes.root}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell>Název</StyledTableCell>
+                        <StyledTableCell>Množství</StyledTableCell>
+                        <StyledTableCell>Cena</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        items.map(item => (
+                            <OrderDetailItemRow item={item} key={item.id} />
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </Paper>
+    );
+};
 
 OrderDetailItems.propTypes = {
     items : PropTypes.arrayOf(OrderItemPropType).isRequired
