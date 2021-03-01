8dd10ba1fb505e6bc3329a58f1be0368b0aa7653
23boro23 <mborovsky@zoznam.sk>
2021-03-01 08:21:04 +0100

    Improve error page

--- frontend/src/components/ErrorPage.jsx
+++ frontend/src/components/ErrorPage.jsx
@@ -17,16 +17,31 @@ const styles = theme => ({
         display : 'block',
         margin : '0 auto'
     },
-    mail : {
-        fontSize : 14
+    info : {
+        fontSize : 14,
+        listStylePosition : 'inside',
+        marginTop : '1rem',
+        paddingLeft : 0
     }
 });
 
 const ErrorPage = ({ classes, middleOfScreen }) => (
     <div className={classes.centered} style={middleOfScreen && { top : '50%', transform : 'translate(0, -50%)'}}>
         <FontAwesomeIcon icon={faDizzy} className={classes.icon} size="7x" />
-        <div>Ups, něco se pokazilo!</div>
-        <div className={classes.mail}>Dejte nám vědět na <ContactEmail /></div>
+        <div>Ups, něco se pokazilo! Co teď?</div>
+        <ol className={classes.info}>
+            <li>Zkuste obnovit stránku (F5)</li>
+            <li>
+                Zkuste promazat cache prohlížeče. Nápověda dle platformy zde:
+                <br />
+                Chrome: <a href="https://support.google.com/accounts/answer/32050?co=GENIE.Platform%3DAndroid&hl=cs" target="_blank">https://support.google.com/accounts/answer/32050?co=GENIE.Platform%3DAndroid&hl=cs</a>
+                <br />
+                IOS - Safari: <a href="https://dotekomanie.cz/2020/03/iphone-a-ipad-jak-vymazat-cache-v-safari/" target="_blank">https://dotekomanie.cz/2020/03/iphone-a-ipad-jak-vymazat-cache-v-safari/</a>
+                <br />
+                MACOS - Safari: <a href="https://www.letemsvetemapplem.eu/2017/06/08/vymazat-cache-safari-ios-i-macos/" target="_blank">https://www.letemsvetemapplem.eu/2017/06/08/vymazat-cache-safari-ios-i-macos/</a>
+            </li>
+            <li>Dejte nám vědět na <ContactEmail /></li>
+        </ol>
     </div>
 );
 
