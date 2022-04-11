6842229ea10102a0752990ecf3ff4c264ec72d3e
23boro23 <mborovsky@zoznam.sk>
2022-04-11 08:55:38 +0200

    Avoid n+1 query for orders

--- backend-parent/backend/src/main/java/cz/zorganizovano/backend/dao/OrderDao.java
+++ backend-parent/backend/src/main/java/cz/zorganizovano/backend/dao/OrderDao.java
@@ -10,13 +10,16 @@ public interface OrderDao extends JpaRepository<Order, Long> {
     @Query("SELECT COUNT(o) FROM Order o WHERE DATE(o.created) = CURDATE()")
     long countTodayOrders();
 
-    @Query("SELECT o FROM Order o WHERE (o.invoiceSent IS NULL OR (?1 = true AND o.invoiceSent IS NOT NULL)) AND (o.storno IS NULL OR (?2 = true AND o.storno IS NOT NULL)) ORDER BY o.created DESC")
+    @Query("SELECT o FROM Order o JOIN FETCH o.customer "
+            + "WHERE (o.invoiceSent IS NULL OR (?1 = true AND o.invoiceSent IS NOT NULL)) "
+            + "AND (o.storno IS NULL OR (?2 = true AND o.storno IS NOT NULL)) "
+            + "ORDER BY o.created DESC")
     List<Order> findOrders(boolean invoiceSent, boolean storno);
     
-    @Query("SELECT o FROM Order o WHERE o.id IN(?1) ORDER BY o.created DESC")
+    @Query("SELECT o FROM Order o JOIN FETCH o.customer WHERE o.id IN(?1) ORDER BY o.created DESC")
     List<Order> findOrdersForReport(List<Long> orderIds);
 
-    @Query("SELECT o FROM Order o WHERE o.paymentReceived IS NULL AND o.storno IS NULL")
+    @Query("SELECT o FROM Order o JOIN FETCH o.customer WHERE o.paymentReceived IS NULL AND o.storno IS NULL")
     List<Order> findUnpaidOrders();
 
     @Query("SELECT DISTINCT year(o.created) AS createdYear FROM Order o ORDER BY createdYear DESC")
